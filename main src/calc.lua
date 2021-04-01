21f2f07ea05e458eedead60be024198532c20637
Developer78-sgyuijhgygwtdwgyhutre45r5t <gamerk5999@gmail.com>
2021-04-01 12:53:58 -0400

    add reset at end of script
    Update calc.lua

--- main src/calc.lua
+++ main src/calc.lua
@@ -12,6 +12,7 @@ print("Enter 2nd degit!")
 local add = io.read("*n")
 print(add,"Is 2nd degit")
 print("answer is !", add1 + add)
+modeadd = "comp"
 end
 if modesel == "sub" then
   modesub = true
@@ -25,6 +26,7 @@ print("Enter 2nd degit!")
 local sub2 = io.read("*n")
 print(sub2,"Is 2nd degit")
 print("answer is !", sub1 - sub2)
+modesub = "comp"
 end
 if modesel == "mul" then
   modemul = true
@@ -38,6 +40,7 @@ print("Enter 2nd degit!")
 local mul2 = io.read("*n")
 print(mul2,"Is 2nd degit")
 print("answer is !", mul1 * mul2)
+modemul = "comp"
 end
 if modesel == "div" then
   modediv = true
@@ -51,4 +54,14 @@ print("Enter 2nd degit!")
 local div2 = io.read("*n")
 print(div2,"Is 2nd degit")
 print("answer is !", div1 / div2)
+modediv = "comp"
 end
+if modediv == "comp" then
+  print("press F6 to reset the program")
+elseif modemul == "comp" then
+ print("press F6 to reset the program")
+elseif modesub == "comp" then
+  print("press F6 to reset the program") 
+elseif modeadd == "comp" then
+   print("press F6 to reset the program")
+  end
